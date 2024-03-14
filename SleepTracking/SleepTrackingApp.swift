77e8564a953b36ea55c4d003e6432cf3492394c0
jldavid <jldavid.mail@gmail.com>
2024-03-14 00:11:54 -0400

    Initial commit

---
+++ SleepTracking/SleepTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepTrackingApp.swift
+//  SleepTracking
+//
+//  Created by JL David on 2024-03-10.
+//
+
+import SwiftUI
+
+@main
+struct SleepTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
